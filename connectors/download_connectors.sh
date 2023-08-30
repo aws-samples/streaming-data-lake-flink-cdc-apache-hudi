75e0c2d588c9eceddddb8f3ee7bb745845bc93e0
Francisco Morillo <fmorillo@amazon.com>
2023-08-30 13:24:20 +0200

    adding connectors script

---
+++ connectors/download_connectors.sh
@@ -0,0 +1,7 @@
+wget https://repo1.maven.org/maven2/org/apache/hudi/hudi-flink1.15-bundle/0.13.1/hudi-flink1.15-bundle-0.13.1.jar
+wget https://repo1.maven.org/maven2/org/datanucleus/datanucleus-core/6.0.4/datanucleus-core-6.0.4.jar
+wget https://repo1.maven.org/maven2/org/apache/flink/flink-s3-fs-hadoop/1.17.1/flink-s3-fs-hadoop-1.17.1.jar
+wget https://repo1.maven.org/maven2/com/ververica/flink-sql-connector-postgres-cdc/2.4.1/flink-sql-connector-postgres-cdc-2.4.1.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-mapreduce-client-core/3.3.6/hadoop-mapreduce-client-core-3.3.6.jar
+wget https://repo1.maven.org/maven2/org/apache/hive/hive-common/2.3.1/hive-common-2.3.1.jar
+wget https://repo1.maven.org/maven2/org/apache/hive/hive-exec/2.3.1/hive-exec-2.3.1.jar
